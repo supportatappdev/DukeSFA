c42f841efc1daf4d5fb918a46128adc546bdaa3d
Support <5upportatappdev@gmail.com>
2017-07-12 18:26:29 +0530

    Initial Checkin

---
+++ config/app.js
@@ -0,0 +1,26 @@
+/**
+ * mymobile3
+ */
+(function () {
+    angular.module('mymobile3', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+        'ngResource',
+         'cgNotify',
+          'oitozero.ngSweetAlert',
+          'infinite-scroll',
+           'ngMap',
+           'doneComponentsSet'
+    ])
+})();  
+
+
+function getBaseURL() {
+	   return location.protocol + "//" + location.hostname + 
+	      (location.port && ":" + location.port) ;
+}; 
+function getAppName(p) {
+   return "/";
+}
+var _appUrl = "http://202.53.13.76:8080";//getBaseURL()+getAppName(window.location.pathname);
