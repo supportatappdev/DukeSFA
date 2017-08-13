47d2105744120d360dc9b7bc2bd999bed8fd0d10
Support <5upportatappdev@gmail.com>
2017-08-13 15:04:24 +0530

    Updated on Sun Aug 13 15:02:27 IST 2017

--- dashboard/ctrl.js
+++ dashboard/ctrl.js
@@ -1,10 +1,41 @@
 /**
- * OrderCtrl - controller
+ * DBCtrl - controller
  */
-function DBCtrl($scope,Cache,$location,AlertService,$http,BSServiceUtil,$modal) {
-        $("body").removeClass("mini-navbar");
-}
 
 angular
     .module('mymobile3')
-    .controller('DBCtrl', ['$scope','Cache','$location','AlertService','$http','BSServiceUtil','$modal',EODCtrl]);
+    .controller('DBCtrl', function DBCtrl(DoneStoreCache,$scope,Cache,$location,AlertService,$http,BSServiceUtil,$modal) {
+        $("body").removeClass("mini-navbar");
+        var _targetStore = DoneStoreCache.create("_keytargetStore","SFSalesTargetViewRef");
+         var _msgStore = DoneStoreCache.create("_keymsgStore","SFMsgDayRef");
+    $scope.x = {};
+    var loadTargets = function(po) {
+            _targetStore.setWhereClause("  executive_code = ? ");
+            _targetStore.setLimit(300);
+            _targetStore.setOffset(0);
+            _targetStore.setGroupBy("prdtype_name");
+            _targetStore.setWhereClauseParams([$scope.salesrepdetials.executive_code]);
+            _targetStore.query().then(function(result) {
+              $scope.targets= result.data;
+               $scope.targetamount = 0;
+               $scope.balamount = 0;
+              for(var k = 0 ; k < $scope.targets.length; k++) {
+                  $scope.targetamount += $scope.targets[k].aug_target_amount;
+                 // $scope.balamount += $scope.targamount;
+              }
+            });
+       }
+     loadTargets(); 
+     var loadMessage = function() {
+         _msgStore.setWhereClause("  id = ? ");
+            _msgStore.setLimit(10);
+            _msgStore.setOffset(0);
+            _msgStore.setWhereClauseParams([3]);
+            _msgStore.query().then(function(result) {
+                if(result.data && result.data[0]) {
+                  $scope.x.message = result.data[0].msg_desc;
+                }
+            });
+     }
+     loadMessage();
+});
