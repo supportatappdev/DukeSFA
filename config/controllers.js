e5f047c0bd2e53952db087c4e5713fa16cad3191
Support <5upportatappdev@gmail.com>
2017-08-19 00:37:16 +0530

    Updated on Sat Aug 19 00:35:44 IST 2017

--- config/controllers.js
+++ config/controllers.js
@@ -5,6 +5,8 @@ angular
     .module('mymobile3')
     .controller('MainCtrl', function MainCtrl(DoneStoreCache,$scope,Cache,$location,AlertService,$http,BSServiceUtil) {
     $scope._appUrl = _appUrl;
+    AlertService.showError("Setup Error","Step 1");
+            
     $scope.pageTitle = "Journey Plan";
     $scope.params = {};
     $scope.params.isStrartDay = false;
